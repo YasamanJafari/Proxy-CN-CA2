be1cae04907f19d84e3bcccd68556bc78ae9eda5
Ssadaf <s.sadaf2277@gmail.com>
2019-03-30 19:16:53 +0430

    This commit will accept connections and receive requests asynchronously

--- proxy.py
+++ proxy.py
@@ -1,21 +1,46 @@
-import asyncio
 import json
 import socket
+import threading
 
 HOST = "192.168.11.2"
 CONFIG_FILE_NAME = "config.json"
+DATA_SIZE = 1024
 
 def createSocket(portNum):
+	threads = []
 	with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-	    s.bind((HOST, portNum))
-	    s.listen()
-	    con, addr = s.accept()
-	    return con, addr
+		s.bind(HOST, portNum)
+		s.listen()
+		while True:
+			con, addr = s.accept()
+			thread = threading.Thread(target = processRequest, args = (con, addr, ))
+			thread.start()
+			threads.append(thread)
+	    
+		for thread in threads:
+			thread.join()
+
+def processRequest(con, addr):
+	data = ""
+	while True:
+		data += con.recv(DATA_SIZE)
+		if not data:
+			break
+	# parsedData = parseHTTP(data)
+	#TODO: 
+	#send request to server
+	#recv response from server
+	#send response to client
+
+#def parseHTTP(data):
+
 
 def readConfig():
 	with open(CONFIG_FILE_NAME) as json_file:  
 		data = json.load(json_file)
 	return data
+
+# async def getRequesst():
 
 if __name__ == "__main__":
 	parsedInfo = readConfig()
