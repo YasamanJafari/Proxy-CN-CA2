9ac76c8404d4a3d10d7a2786d7069dabd192669e
Yasaman <ys.jafari98@gmail.com>
2019-03-30 16:30:33 +0430

    This commit will add reading from file, parsing config, and creating socket.

--- proxy.py
+++ proxy.py
@@ -1,1 +1,22 @@
 import asyncio
+import json
+import socket
+
+HOST = '127.0.0.1'
+CONFIG_FILE_NAME = "config.json"
+
+def createSocket(portNum):
+	with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+	    s.bind((HOST, portNum))
+	    s.listen()
+	    con, addr = s.accept()
+	    return con, addr
+
+def readConfig():
+	with open(CONFIG_FILE_NAME) as json_file:  
+		data = json.load(json_file)
+	return data
+
+if __name__ == "__main__":
+	parsedInfo = readConfig()
+	createSocket(parsedInfo["port"])
